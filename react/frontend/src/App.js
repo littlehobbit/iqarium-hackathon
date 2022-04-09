0cf821f6f5e2406bb388ed8c5fdab8de5160cd8e
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2022-04-09 07:55:01 +0300

    before file clear

--- react/frontend/src/App.js
+++ react/frontend/src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './App.css';
 import PageCheckCategory from "./PageCheckCategory/PageCheckCategory";
 import PageTextControlNeuron from "./PageControlTextNeuron/PageTextControlNeuron";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
 
 function App() {
     return (
-        <div className="App">
-            {/*<PageTextControlNeuron/>*/}
-            <PageCheckCategory/>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/text-control-neuron" element={
+                        <PageTextControlNeuron/>
+                    }/>
+
+                    <Route path="/page-check-category"  element={
+                        <PageCheckCategory/>
+                    }/>
+
+            </Routes>
         </div>
-    );
+</BrowserRouter>
+)
+    ;
 }
 
 export default App;
--- react/frontend/src/Common/DetailedCard/DetailedCard.js
+++ react/frontend/src/Common/DetailedCard/DetailedCard.js
@@ -2,22 +2,34 @@ import React from 'react';
 import s from "./DetailedCard.module.css";
 
 function DetailedCard(props) {
+    func();
     return (
-        <div className={s.detailed_card} onClick={() => {
+        <div>
+            <div className={s.detailed_card} onClick={() => {
 
-        }}>
-            <div className={s.body}>
-                <span className={s.title}> {props.title}</span>
-                <span className={s.category}> {`категория: ${props.category}`}</span>
-                <span className={s.sub_category}> {`подкатегория: ${props.category}`}</span>
-                <span className={s.sender}> {props.sender}</span>
-            </div>
-            <div className={s.bottom_layer}>
+            }}>
+                <div className={s.bottom_layer}>
+
+                </div>
+                <div className={s.body}>
+                    <span className={s.title}> {props.title}</span>
+                    <span className={s.category}> {`категория: ${props.category}`}</span>
+                    <span className={s.sub_category}> {`подкатегория: ${props.category}`}</span>
+                    <span className={s.sender}> {props.sender}</span>
+                </div>
 
             </div>
+
         </div>
-    );
+
+    )
+
 }
 
+
+async function func() {
+    let otvet = await (await fetch('http://26.120.212.37:3000/img-req/request/1')).json();
+    console.log(otvet);
+}
 
 export default DetailedCard;
